73e736ec18019d99107734b26aed747ec03aee95
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2018-02-15 01:35:26 +0000

    Fix undefined variable bug

--- test/nodegen/node-red-contrib-swagger-petstore/node_spec.js
+++ test/nodegen/node-red-contrib-swagger-petstore/node_spec.js
@@ -36,6 +36,168 @@ describe('node-red-contrib-swagger-petstore', function () {
             done();
         });
     });
+    it('should handle addPet()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "addPet"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.payload.should.eql({
+                        "id": 4513,
+                        "category": {
+                            "id": 4649,
+                            "name": "string"
+                        },
+                        "name": "doggie",
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 2525,
+                                "name": "string"
+                            }
+                        ],
+                        "status": "available"
+                    });
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({
+                payload: {
+                    "id": 4513,
+                    "category": {
+                        "id": 4649,
+                        "name": "string"
+                    },
+                    "name": "doggie",
+                    "photoUrls": [
+                        "string"
+                    ],
+                    "tags": [
+                        {
+                            "id": 2525,
+                            "name": "string"
+                        }
+                    ],
+                    "status": "available"
+                }
+            });
+        });
+    });
+    it('should handle updatePet()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "updatePet"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.payload.should.eql({
+                        "id": 4513,
+                        "category": {
+                            "id": 5963,
+                            "name": "string"
+                        },
+                        "name": "doggie",
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 3341,
+                                "name": "string"
+                            }
+                        ],
+                        "status": "available"
+                    });
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({
+                payload: {
+                    "id": 4513,
+                    "category": {
+                        "id": 5963,
+                        "name": "string"
+                    },
+                    "name": "doggie",
+                    "photoUrls": [
+                        "string"
+                    ],
+                    "tags": [
+                        {
+                            "id": 3341,
+                            "name": "string"
+                        }
+                    ],
+                    "status": "available"
+                }
+            });
+        });
+    });
+    it('should handle findPetsByStatus()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "findPetsByStatus", findPetsByStatus_status: "available"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.payload.should.containEql({
+                        "id": 4513,
+                        "category": {
+                            "id": 5963,
+                            "name": "string"
+                        },
+                        "name": "doggie",
+                        "photoUrls": [
+                            "string"
+                        ],
+                        "tags": [
+                            {
+                                "id": 3341,
+                                "name": "string"
+                            }
+                        ],
+                        "status": "available"
+                    });
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({});
+        });
+    });
+    it('should handle updatePetWithForm()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "updatePetWithForm", updatePetWithForm_petId: "4513", updatePetWithForm_name: "pending doggie", updatePetWithForm_status: "pending"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.should.have.property('payload', 'foo');
+                    msg.should.have.property('topic', 'bar');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({ payload: "foo", topic: "bar" });
+        });
+    });
     it('should handle getInventory()', function (done) {
         var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "getInventory"},
                     {id: "n2", type: "swagger-petstore-service"},
@@ -46,6 +208,62 @@ describe('node-red-contrib-swagger-petstore', function () {
             n3.on('input', function (msg) {
                 try {
                     msg.payload.should.have.property('available');
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({});
+        });
+    });
+    it('should handle createUser()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "createUser"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.payload.should.eql({
+                        "id": 8110,
+                        "username": "My user name",
+                        "firstName": "My first name",
+                        "lastName": "My last name",
+                        "email": "My e-mail address",
+                        "password": "My password",
+                        "phone": "My phone number",
+                        "userStatus": 0
+                    });
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+            n1.receive({
+                payload: {
+                    "id": 8110,
+                    "username": "My user name",
+                    "firstName": "My first name",
+                    "lastName": "My last name",
+                    "email": "My e-mail address",
+                    "password": "My password",
+                    "phone": "My phone number",
+                    "userStatus": 0
+                }
+            });
+        });
+    });
+    it('should handle loginUser()', function (done) {
+        var flow = [{id: "n1", type: "swagger-petstore", wires: [["n3"]], service: "n2", method: "loginUser", loginUser_username: "My user name", loginUser_password: "My password"},
+                    {id: "n2", type: "swagger-petstore-service"},
+                    {id: "n3", type: "helper"}];
+        helper.load(swaggerNode, flow, function () {
+            var n1 = helper.getNode('n1');
+            var n3 = helper.getNode('n3');
+            n3.on('input', function (msg) {
+                try {
+                    msg.payload.should.startWith('logged in user session:');
                     done();
                 } catch (e) {
                     done(e);
