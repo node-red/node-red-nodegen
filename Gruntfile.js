05c665a30fad70b56a6d92b35ed38f4287388341
Kazuhito Yokoi <kazuhito.yokoi.nx@hitachi.com>
2018-05-31 13:26:05 +0900

    Support Windows to run grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,29 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         shell: {
-            generate_nodes: {
-                command: [
-                    'mkdir nodegen',
-                    'node bin/node-red-nodegen.js samples/handle-clearInterval.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/handle-setInterval.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/handle-setTimeout.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/log-a-Warning-Message.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/log-an-Error-Message.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/log-an-Info-Message.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js samples/lower-case.js -o ./nodegen',
-                    'node bin/node-red-nodegen.js http://petstore.swagger.io/v2/swagger.json -o ./nodegen'
-                ].join(';')
+            generateNode_handleClearInterval: {
+                command: 'node bin/node-red-nodegen.js samples/handle-clearInterval.js -o ./nodegen'
+            },
+            generateNode_handleSetInterval: {
+                command: 'node bin/node-red-nodegen.js samples/handle-setInterval.js -o ./nodegen'
+            },
+            generateNode_handleSetTimeout: {
+                command: 'node bin/node-red-nodegen.js samples/handle-setTimeout.js -o ./nodegen'
+            },
+            generateNode_logAWarningMessage: {
+                command: 'node bin/node-red-nodegen.js samples/log-a-Warning-Message.js -o ./nodegen'
+            },
+            generateNode_logAnErrorMessage: {
+                command: 'node bin/node-red-nodegen.js samples/log-an-Error-Message.js -o ./nodegen'
+            },
+            generateNode_logAnInfoMessage: {
+                command: 'node bin/node-red-nodegen.js samples/log-an-Info-Message.js -o ./nodegen'
+            },
+            generateNode_lowerCase: {
+                command: 'node bin/node-red-nodegen.js samples/lower-case.js -o ./nodegen'
+            },
+            generateNode_swaggerPetstore: {
+                command: 'node bin/node-red-nodegen.js http://petstore.swagger.io/v2/swagger.json -o ./nodegen'
             }
         },
         simplemocha: {
@@ -32,10 +43,10 @@ module.exports = function (grunt) {
             }
         }
     });
+    grunt.file.mkdir('nodegen');
     grunt.loadNpmTasks('grunt-shell');
     grunt.loadNpmTasks('grunt-simple-mocha');
     grunt.loadNpmTasks('grunt-mocha-istanbul');
     grunt.registerTask('default', ['shell', 'simplemocha']);
     grunt.registerTask('coverage', 'Run Istanbul code test coverage task', ['shell', 'mocha_istanbul']);
 };
-
