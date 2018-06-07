63cec3d8a55d2f5441061386354ecb0a1499be34
Yuma Matsuura <yuma.matsuura.uj@hitachi-solutions.com>
2018-06-07 13:50:07 +0900

    update timeout options

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
         },
         simplemocha: {
             options: {
-                timeout: 3000
+                timeout: 10000
             },
             all: {
                 src: [ 'test/**/*_spec.js' ]
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
         },
         mocha_istanbul: {
             options: {
-                timeout: 3000
+                timeout: 10000
             },
             coverage: {
                 src: [ 'test/**/*_spec.js' ]
